8b64ab5cd84b84688dd26e966526f4e83e0af623
Zach Bentzinger <bentzingerz@gmail.com>
2022-08-28 10:02:07 -0600

    Initial project creation

---
+++ README.md
@@ -0,0 +1,5 @@
+# CMS Spikes
+This repo houses my "hello worlds" and test benches for reviewing Content Management Systems. 
+
+## Current List:
+- ./django_wagtail_spike: Test environment for [Wagtail](https://wagtail.org)
---
+++ django_wagtail_spike/django_wagtail_spike/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_vi*g+oikjznf24=#qne6f4fb3k+!xv!rwueubk3#ewh3#z1vg"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
